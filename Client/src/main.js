47742008f88b70a4d3cc1db3e3cd1ee4bf5e85d0
Karl-Maison <karljoyeux@gmail.com>
2023-06-01 20:38:05 +0200

    begin new integration with d3

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router/index.js';
-import mitt from 'mitt';
+
+
 
 const app = createApp(App)
-app.use(router);
+
 app.config.globalProperties.emitter = mitt();
 app.mount('#app')
